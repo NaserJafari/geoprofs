525c608a9acd1ca1a578654aaa01de9ca7547cdb
xLeonPlays <1195150@student.roc-nijmegen.nl>
2023-10-04 15:14:02 +0200

    form fix

---
+++ geoprofs/geoprofs/Pages/Verlof.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+
+public class VerlofModel : PageModel
+{
+    public string Reden { get; set; }
+    public string Beschrijving { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public TimeSpan StartTime { get; set; }
+    public TimeSpan EndTime { get; set; }
+}
