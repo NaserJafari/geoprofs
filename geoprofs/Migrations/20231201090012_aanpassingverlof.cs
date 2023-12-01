dbe770a03c5e34057f2fd714f9ddd20a54d6b148
Naser Jafari <naserjafari5002@gmail.com>
2023-12-01 11:08:32 +0100

    verlof goedkeuren en afwijzen werkt nu

---
+++ geoprofs/Migrations/20231201090012_aanpassingverlof.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace geoprofs.Migrations
+{
+    public partial class aanpassingverlof : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "VerlofReden",
+                table: "Verlof",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ApproveDeny",
+                table: "Verlof",
+                type: "int",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ApproveDeny",
+                table: "Verlof");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "VerlofReden",
+                table: "Verlof",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
--- geoprofs/Models/Verlof.cs
+++ geoprofs/Models/Verlof.cs
@@ -1,17 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace geoprofs.Models
 {
 
     public class Verlof
     {
         public int VerlofId { get; set; }
-        public int VerlofReden { get; set; }
-        public string VerlofOmschrijving { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public DateTime BeginVerlof { get; set; }
-        public DateTime EindVerlof { get; set; }
-        public int VerlofResterend { get; set; }
-        public string rol { get; set; }
-        public int VerlofStatus { get; set; }
+        public string? VerlofReden { get; set; }
+        public string? VerlofOmschrijving { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime? BeginVerlof { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime? EindVerlof { get; set; }
+        public int? VerlofResterend { get; set; }
+        public string? rol { get; set; }
+        public int? VerlofStatus { get; set; }
+        public int? ApproveDeny { get; set; }
     }
 }
