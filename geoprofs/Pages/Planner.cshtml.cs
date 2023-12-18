51c40edb3b5848a7fb242a1e3ea35dd1861d1959
xLeonPlays <1195150@student.roc-nijmegen.nl>
2023-12-18 11:38:13 +0100

    verlofsaldo kan worden berekend, begin planner

---
+++ geoprofs/Pages/Planner.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace geoprofs.Pages
+{
+    public class PlannerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
